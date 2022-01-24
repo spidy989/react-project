ecf1c9f474cf1a5a3657134f282f67522c4b6037
spidy989 <98208157+spidy989@users.noreply.github.com>
2022-01-24 17:05:09 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import './index.css';
+import * as App from './App.js';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App.navbar title="REACT" />
+    <App.card title="Hunger Report" text="This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer." />
+    <App.card title="Finance Record" text="This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer." />
+    <App.pagination />
+  </React.StrictMode>,
+  document.getElementById('app')
+);
